6f9ba2e82d89419bb03977c04c396acfbacd3d56
MrJuise <126503964+MrJuise@users.noreply.github.com>
2025-12-27 12:11:22 +0300

    Fix URL path for message search endpoint

--- points/urls.py
+++ points/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('points/', PointCreateAPIView.as_view(), name='point-create'),
     path('points/messages/', MessageCreateAPIView.as_view(), name='message-create'),
     path('points/search/', PointSearchAPIView.as_view(), name='point-search'),
-    path('message/search/', MessageSearchAPIView.as_view(), name='message-search'),
+    path('messages/search/', MessageSearchAPIView.as_view(), name='message-search'),
 ]
